d20a13f4827cc1c2727cb42897586b071cb7d3e9
Jayromberg <jayro.dev@gmail.com>
2023-05-10 19:02:37 -0300

    feat: Component Icon

--- src/screens/HomeScreen/patterns/Feed/Feed.tsx
+++ src/screens/HomeScreen/patterns/Feed/Feed.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Box from "@src/components/Box/Box";
 import Text from "@src/components/Text/Text";
+import Icon from '@src/components/Icon/Icon';
 
 interface FeedProps {
   children: React.ReactNode;
@@ -19,6 +20,8 @@ export default function Feed({ children }: FeedProps) {
 Feed.Header = () => {
   return (
     <Box>
+      <Icon name='github' />
+      <Icon name='default_icon' />
       <Text>
         Feed Header
       </Text>
