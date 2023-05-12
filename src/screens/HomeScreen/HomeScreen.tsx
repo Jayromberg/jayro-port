37b100efb2350d567c64bebea6253354fd4c7cd6
Jayromberg <jayro.dev@gmail.com>
2023-05-12 20:09:37 -0300

    refactor: Background

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,27 +1,35 @@
 import Box from "@src/components/Box/Box";
+import { useTheme } from "@src/theme/ThemeProvider";
 import Text from "@src/components/Text/Text";
 import Background from "./patterns/Background/Background";
 import Feed from "./patterns/Feed/Feed";
 import Menu from "./patterns/Menu/Menu";
 import Footer from "./patterns/Footer/Footer";
 
+
 export default function HomeScreen() {
+  const theme = useTheme();
   return (
     <Box
       tag="main"
+      styleSheet={{
+        backgroundColor: theme.colors.neutral.x000,
+        flex: 1,
+        alignItems: 'center',
+      }}
     >
       <Background />
-      <Menu />
+      {/* <Menu /> */}
       <Feed>
-        <Feed.Header />
-        <Text
+        {/* <Feed.Header /> */}
+        {/* <Text
           variant='display1'
         >
           Últimas Atualizações
-        </Text>
-        <Feed.Posts />
+        </Text> */}
+        {/* <Feed.Posts /> */}
       </Feed>
-      <Footer />
+      {/* <Footer /> */}
     </Box>
   )
 }
