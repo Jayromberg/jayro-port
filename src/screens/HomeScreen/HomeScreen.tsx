ef1f89bda32d7a075f1a9045fff699d63cc31103
Jayro <95861623+Jayromberg@users.noreply.github.com>
2023-05-12 21:12:58 -0300

    Layout (#7)
    * feat: Link
    
    * fix: Correção do bug das tags no NextLink
    
    * feat: Component Button construido
    
    * feat: Button
    
    * refactor: Comentarios apagados
    
    * refactor: Background
    
    * refactor: Footer
    
    * refactor: Feed
    
    * refactor: Eslint
    
    * refactor: Menu
    
    * refactor: Menu - remocao do import theme

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,18 +1,30 @@
 import Box from "@src/components/Box/Box";
+import { useTheme } from "@src/theme/ThemeProvider";
 import Text from "@src/components/Text/Text";
 import Background from "./patterns/Background/Background";
 import Feed from "./patterns/Feed/Feed";
 import Menu from "./patterns/Menu/Menu";
 import Footer from "./patterns/Footer/Footer";
 
+
 export default function HomeScreen() {
+  const theme = useTheme();
   return (
     <Box
       tag="main"
+      styleSheet={{
+        backgroundColor: theme.colors.neutral.x000,
+        flex: 1,
+        alignItems: 'center',
+      }}
     >
-      <Background />
       <Menu />
+      <Background />
       <Feed>
+        <Feed.Header />
+      </Feed>
+      <Footer />
+      {/* <Feed>
         <Feed.Header />
         <Text
           variant='display1'
@@ -20,8 +32,7 @@ export default function HomeScreen() {
           Últimas Atualizações
         </Text>
         <Feed.Posts />
-      </Feed>
-      <Footer />
+      </Feed> */}
     </Box>
   )
 }
